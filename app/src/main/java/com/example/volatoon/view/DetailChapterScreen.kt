9ac875f9ce3a25cfc4df18b2dfe79bff518d5173
Rafael Simarmata <80381531+Rafaellsimarmata@users.noreply.github.com>
2024-11-25 15:15:33 +0700

    fixed image size

--- app/src/main/java/com/example/volatoon/view/DetailChapterScreen.kt
+++ app/src/main/java/com/example/volatoon/view/DetailChapterScreen.kt
@@ -1,9 +1,11 @@
 package com.example.volatoon.view
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
@@ -11,8 +13,10 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -22,11 +26,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.SubcomposeAsyncImage
 import coil.compose.rememberAsyncImagePainter
 import com.example.volatoon.viewmodel.ComicViewModel
 
@@ -35,7 +42,8 @@ fun DetailChapterScreen(
     viewState : ComicViewModel.DetailChapterState
 ){
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
             .padding(10.dp)
             .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally
@@ -71,16 +79,15 @@ fun DetailChapterScreen(
 
                 Spacer(modifier = Modifier.height(10.dp))
 
-                Column {
-                    chapter.images.forEachIndexed() { index, chapterImg ->
-                        Image(
-                            painter = rememberAsyncImagePainter(model = chapterImg),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .aspectRatio(1f)
-                        )
-                    }
+                chapter.images.forEachIndexed { index, chapterImg ->
+                    SubcomposeAsyncImage(
+                        model = chapterImg,
+                        contentDescription = "Chapter Image $index",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight(), // Ensures it respects its intrinsic height
+                        contentScale = ContentScale.FillWidth // Ensures it fills the width without cropping
+                    )
                 }
             }
         }
